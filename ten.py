f9072f138f2f27986c1a2287c14c6dcad786e7c9
unknown <olaniyivctr@gmail.com>
2025-03-07 12:56:50 +0000

    second commit

--- ten.py
+++ ten.py
@@ -1,16 +1,3 @@
-# import streamlit as st
-# import trimesh
-# import pyrender
-# import time 
-
-# st.write("Hello world")
-# time.sleep(5)
-# fuze_trimesh = trimesh.load('examples/models/coffee_cup_obj.obj')
-# mesh = pyrender.Mesh.from_trimesh(fuze_trimesh)
-# scene = pyrender.Scene()
-# scene.add(mesh)
-# st.pydeck_chart(pyrender.Viewer(scene, use_raymond_lighting=True))
-
 import streamlit as st
 import base64
 
